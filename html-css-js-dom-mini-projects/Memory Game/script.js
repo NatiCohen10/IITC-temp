f3b06fecbfe4c4d5c35483d959839d91d40574be
NatiCohen10 <nati1236nati@gmail.com>
2024-04-20 17:41:38 +0300

    added comments for explenation

--- html-css-js-dom-mini-projects/Memory Game/script.js
+++ html-css-js-dom-mini-projects/Memory Game/script.js
@@ -15,7 +15,7 @@ let startTime;
 let timerInterval;
 
 // //////////////////////////////
-// Timer function(credit to elad)
+// Timer function(credit to Elad)
 // /////////////////////////////
 function startTimer() {
     startTime = Date.now();
@@ -35,7 +35,6 @@ function startTimer() {
   }
   function stopTimer() {
     clearInterval(timerInterval);
-    console.log("Timer stopped"); // Add a log message for debugging
 
   }
 
@@ -43,22 +42,26 @@ function startTimer() {
 ///////////////////
 // game functions
 //////////////////
+// after submitting name, goes to size select page
 function goToInitPage(){
     document.querySelector('.getDetails').style.display = 'none'
     document.querySelector('.initPage').style.display = 'flex'
 }
 
 
-
+// create board by the size the user chose.
 function createBoard(sizeByUser) {
+    // make the board in the style of a grid
   document.querySelector(".board").style.gridTemplateColumns =
     "repeat(" + sizeByUser + ", 85px)";
+    // if the board is 4x4 the number of cards is 16
   numberOfCards = sizeByUser ** 2;
   document.querySelector(".initPage").style.display = "none";
   document.querySelector(".board").style.display = 'grid';
   document.querySelector('.utilityButtons').style.display = 'flex';
   document.querySelector('.top').style.display = 'flex';
   document.querySelector('#winMessage').innerText = '';
+    // adds 1 card for the available number of cards 
   for (let i = 1; i <= numberOfCards; i++) {
     board.innerHTML += `<div class="card" id = "card${i}">
         <button class="gameButton" id="button${i}" onclick="revealCard(${i})"></button></div>`;
@@ -66,6 +69,7 @@ function createBoard(sizeByUser) {
   }
   gameCards = createCardArray();
   assignPicturesToCards();
+    // cards dissappear after some time, dependant on board size
   setTimeout(hideCards, 1000 * (sizeByUser / 2));
 
 
@@ -76,7 +80,7 @@ function createCardArray() {
   let pictureArray = [];
   let functionResultArray = [];
 
-  // Populate pictureArray with unique picture IDs
+  // Populate pictureArray with random picture IDs
   while (pictureArray.length < numberOfCards / 2) {
     let randomPictureId = Math.floor(Math.random() * 21) + 1;
     if (!pictureArray.includes(randomPictureId)) {
@@ -97,17 +101,18 @@ function createCardArray() {
     let randomCard1Id = cardArray.pop();
     let randomCard2Id = cardArray.pop();
     let randomPictureId = pictureArray.pop();
-
+    // add the card and picture ids to an object and pushing it to an array, used to iterate over
     functionResultArray.push({
       card1Id: randomCard1Id,
       card2Id: randomCard2Id,
       pictureId: randomPictureId,
     });
   }
-
+    // the list of cards for me to cheat
   console.log(functionResultArray);
   return functionResultArray;
 }
+
 
 function updateMovesAndMistakes() {
     document.querySelector("#gameMoves").textContent = "Moves: " + moves;
@@ -115,6 +120,7 @@ function updateMovesAndMistakes() {
       "Mistakes: " + mistakes;
   }
 
+//   iterates over an array of objects, assigns 1 random picture id to the card ids that are in the same object as them
 function assignPicturesToCards() {
   for (let obj of gameCards) {
     document.querySelector(
@@ -126,6 +132,7 @@ function assignPicturesToCards() {
   }
 }
 
+
 function hideCards() {
   for (let i = 1; i <= numberOfCards; i++) {
     document.querySelector(`#img${i}`).style.display = "none";
@@ -134,23 +141,26 @@ function hideCards() {
   startTimer()
   startCount = new Date().getTime(); 
 }
-
+// when pressing a button this function is run
 function revealCard(cardId) {
+    // if the counter is 0, assigns openCard1Id the pressed card id
     if (openCardCounter === 0) {
         openCardCounter++;
         openCard1Id = cardId;
         document.querySelector(`#img${cardId}`).style.display = "block";
         document.querySelector(`#button${cardId}`).style.display = "none";
+        // if its 1 assigns the openCard2Id the pressed card id
     } else if (openCardCounter === 1) {
         openCardCounter++;
         openCard2Id = cardId;
         document.querySelector(`#img${cardId}`).style.display = "block";
         document.querySelector(`#button${cardId}`).style.display = "none";
         moves++
-        
+        // if the cards are the same +1 to score
         if (checkSimilarCards()) {
             score++;
             updateMovesAndMistakes()
+            // if the score is the number of cards/2 (number of total pairs) then the player won
             if (score === numberOfCards / 2) {
                 document.querySelector('#winMessage').innerText = 'You won!'
                 const endCount = new Date().getTime();
@@ -159,6 +169,7 @@ function revealCard(cardId) {
                 stopTimer()
             }
         }
+        // if the cards arent the same
         else{
             mistakes++
             disableAllButtons()
@@ -170,9 +181,11 @@ function revealCard(cardId) {
         openCardCounter = 0;
     }
 }
-
+// checks if the cards are similar
 function checkSimilarCards(){
+    // iterates over the list of objects
     for(let obj of gameCards){
+        // if the id of the cards in the object is the same as the ids of the pressed cards
         if((obj.card1Id == openCard1Id && obj.card2Id == openCard2Id) ||
            (obj.card1Id == openCard2Id && obj.card2Id == openCard1Id)){
             return true;
@@ -180,16 +193,18 @@ function checkSimilarCards(){
     }
     return false;
 }
-
+// disable buttons if the cards arent the same
 function disableAllButtons(){
     let buttonList = document.querySelectorAll('.gameButton');
+    // iterates over every button and disables the onclick function
     for(let button of buttonList){
         button.onclick = '';
     }
 }
-
+// enables buttons after disabling them
 function enableAllButtons(){
     let buttonList = document.querySelectorAll('.gameButton');
+    // iterates over every button and gives it the revealCard function onclick
     for(let buttonIndex = 1;buttonIndex<=buttonList.length;buttonIndex++){
         buttonList[buttonIndex-1].onclick = function () {
             revealCard(buttonIndex);
@@ -197,6 +212,7 @@ function enableAllButtons(){
     }
 }
 
+// if the cards are mistaken it flips the cards after 1 second
 function flipCards(){
     document.querySelector(`#button${openCard1Id}`).style.display = 'block';
     document.querySelector(`#img${openCard1Id}`).style.display = 'none';
@@ -205,17 +221,17 @@ function flipCards(){
     enableAllButtons()
 }
 
-
+// if the player won it adds their stats to the table
 function addGameHistory() {
     const gameHistory = {
         name: playerName.value,
         numberOfMoves: moves,
         numberOfMistakes: mistakes,
-        time: totalTime, // Assuming totalTime is correctly calculated
+        time: totalTime, 
     };
     gamesHistory.push(gameHistory);
 }
-
+// on button click it creates the history table
 function showHistory() {
     const tableOfHistory = document.querySelector('#historyTable');
     tableOfHistory.innerHTML = '<tr><th>Player name</th><th>Number of moves</th><th>Number of mistakes</th><th>Time taken</th></tr>';
@@ -224,7 +240,7 @@ function showHistory() {
         tableOfHistory.innerHTML += historyEntry;
     }
 }
-
+// resets all the vars when the new game button is pressed
 function newGame(){
     openCardCounter = 0;
     openCard1Id = 0;
