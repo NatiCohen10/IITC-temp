2c7ee3f6b348fb5947c59f46398158d8b73eb34f
NatiCohen10 <nati1236nati@gmail.com>
2024-05-06 08:54:15 +0300

    added quiz exercise

---
+++ Exc 7/script.js
@@ -0,0 +1,71 @@
+let correctAnswers = ['d', 'b', 'd', 'b', 'd', 'b', 'c', 'b', 'b', 'c'];
+let key = 'quiz'
+let userAnswer =
+localStorage.getItem(key)!==null?JSON.parse(localStorage.getItem(key)) : []
+
+
+console.log(userAnswer);
+
+function checkAnswer(questionNumber, answer){
+    let questionIndex = questionNumber - 1
+    userAnswer[questionIndex] = answer
+    let JsonAnswers = JSON.stringify(userAnswer)
+    localStorage.setItem(key, JsonAnswers)
+    if(localStorage.getItem(key) !== null){
+        
+    }
+    console.log(userAnswer); 
+    colorAnswersBlack(questionNumber)
+    document.getElementById(`q${questionNumber}${answer}`).style.color = 'blue'
+    
+}
+colorAnswersBlueOnInit()
+function colorAnswersBlueOnInit(){
+    if(userAnswer.length !== 0){
+        for(let answer = 0; answer <userAnswer.length;answer++){
+            let questionNumber = answer + 1
+            document.getElementById(`q${questionNumber}${userAnswer[answer]}`).style.color = 'blue'
+        }
+    }
+}
+
+function colorAnswersBlack(questionNumber){
+    let options = document.getElementById(`q${questionNumber}`).querySelectorAll('li');
+    options.forEach(option => option.style.color = '');
+}
+
+function disableOnclick(){
+    for(let questionNumber = 1; questionNumber <= correctAnswers.length; questionNumber++){
+        let options = document.getElementById(`q${questionNumber}`).querySelectorAll('li');
+        options.forEach(option => option.onclick = null);
+    }
+
+}
+function submitAnswers(){
+    if(userAnswer.length === correctAnswers.length){
+        disableOnclick()
+        for(let answer = 0; answer<(userAnswer.length);answer++){
+            let questionNumber = answer+1
+            if(userAnswer[answer] === correctAnswers[answer]){
+                colorAnswersBlack(questionNumber)
+                document.getElementById(`q${questionNumber}${userAnswer[answer]}`).style.color = 'green'
+            }
+            else{
+                colorAnswersBlack(questionNumber)
+                document.getElementById(`q${questionNumber}${userAnswer[answer]}`).style.color = 'red'
+            }
+        }}
+    else{
+        alert('You didnt answer all of the questions yet!')
+    }
+}
+
+function clearStorage(){
+    localStorage.clear()
+    userAnswer = []
+    for(let questionNumber = 1; questionNumber <= correctAnswers.length; questionNumber++){
+        let options = document.getElementById(`q${questionNumber}`).querySelectorAll('li');
+        options.forEach(option => option.style.color = '');
+    }
+    console.log(userAnswer);
+}
